5229bdcf1af76414f7507e219968bf3cd7b6e518
nexydojn1k <yulchacha1904@gmail.com>
2024-10-12 14:07:11 +0300

    Исправленно

--- Lesson1/layouttype/src/main/java/ru/mirea/vasilenkoya/layouttype/MainActivity.java
+++ Lesson1/layouttype/src/main/java/ru/mirea/vasilenkoya/layouttype/MainActivity.java
@@ -6,11 +6,12 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,21 +19,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        // Устанавливаем, что система не будет управлять внутренними отступами окна
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
         setContentView(R.layout.activity_main);
+
+        // Настраиваем отступы для основного layout
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // Инициализация TextView и установка текста
         TextView tv = findViewById(R.id.textViewStudent);
-        TextView myTextView = (TextView) findViewById(R.id.textView);
+        TextView myTextView = findViewById(R.id.textView);
         myTextView.setText("New text in MIREA");
 
+        // Инициализация Button и установка текста
         Button button = findViewById(R.id.button);
         button.setText("MireaButton");
 
+        // Инициализация CheckBox и установка состояния
         CheckBox checkBox = findViewById(R.id.checkBox);
         checkBox.setChecked(true);
     }
