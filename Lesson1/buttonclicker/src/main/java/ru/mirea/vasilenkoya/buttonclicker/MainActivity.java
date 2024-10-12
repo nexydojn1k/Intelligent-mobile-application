43efd303e2392089ad6d00a63252fd0ba9c947ce
nexydojn1k <yulchacha1904@gmail.com>
2024-10-12 14:11:43 +0300

    :)

--- Lesson1/buttonclicker/src/main/java/ru/mirea/vasilenkoya/buttonclicker/MainActivity.java
+++ Lesson1/buttonclicker/src/main/java/ru/mirea/vasilenkoya/buttonclicker/MainActivity.java
@@ -5,50 +5,49 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.ChecksSdkIntAtLeast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textViewStudent;
+    private TextView tvOut;  // Изменил имя на существующий TextView
     private Button btnWhoAmI;
-    private Button btnItIsNotMe ;
+    private Button btnItIsNotMe;
     private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewStudent = findViewById(R.id.textViewStudent);
+
+        // Инициализация TextView и кнопок
+        tvOut = findViewById(R.id.tvOut);  // Обновлено на существующий ID
         btnWhoAmI = findViewById(R.id.btnWhoAmI);
         btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
         checkBox = findViewById(R.id.checkBox);
 
+        // Листенер для кнопки btnWhoAmI
         View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewStudent.setText("Мой номер по списку № 3");
+                tvOut.setText("Мой номер по списку № 3");
             }
         };
 
         btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
 
-        EdgeToEdge.enable(this);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.textViewStudent), (v, insets) -> {
+        // Настройка окна Insets для tvOut
+        ViewCompat.setOnApplyWindowInsetsListener(tvOut, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
 
-    public void onMyButtonClick(View view)
-    {
-        textViewStudent.setText("Это не я сделал");
-        checkBox.isChecked();
+    public void onMyButtonClick(View view) {
+        tvOut.setText("Это не я сделал");
+        checkBox.isChecked(); // Возможно, вы хотите добавить действие с чекбоксом
     }
 }
